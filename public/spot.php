1e5a0a73d5f6a90f8b1450b7596049d992e2011c
GabJimz <46873801+GabJimz@users.noreply.github.com>
2020-12-04 03:25:40 +0100

    spot.php fonctionnel

---
+++ public/spot.php
@@ -0,0 +1,33 @@
+<?php
+
+session_start();
+
+include("../utils/messages.php");
+include("../utils/sendFunctions.php");
+include("../utils/db_connect.php");
+
+if($_SERVER['REQUEST_METHOD'] != "GET") {
+    displayMethodNotAllowed();
+}
+else{
+    $bdd = init_db();
+
+    if (isset($_GET['longitude']) && isset($_GET['latitude']))
+    {
+        $req=$bdd->prepare('SELECT * FROM spot WHERE latitude=:latitude AND longitude=:longitude');
+        $req->execute(array('latitude' => $_GET['latitude'], 'longitude' => $_GET['longitude']));
+        $result=$req->fetchAll(PDO::FETCH_ASSOC);
+    
+        if (count($result) == 0){
+            showError(200, "Aucun resultat pour cette recherche");
+        }        
+        else{
+            deliver_response(200, "Bien recu", $result);
+        }
+    }
+    else{
+        displayBadRequest();
+    }
+}
+
+?>
