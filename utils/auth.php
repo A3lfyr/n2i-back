4eaccfa0aa502d9fb5fea868bb62e65b49bce6bd
vince-stri <acilav.univtlse@gmail.com>
2020-12-04 02:17:11 +0100

    auth par token

--- utils/auth.php
+++ utils/auth.php
@@ -21,4 +21,26 @@
         }
         return 0;
     }
+    /**
+     * Fonction d'authentification d'un utilisateur par token
+     * @param id identifiant 
+     * @param token token 
+     * @return id de l'utilisateur ou 0 en cas d'échec de l'authentification
+     */
+    function account_auth_token($login, $token){
+        $bdd = init_db();
+        if ($bdd==null){
+            return false;
+        }
+        $query = 'SELECT iduser FROM account WHERE userName=? AND token=?';
+        $stmt = $bdd->prepare($query);
+        $stmt->execute(array($login, $token));
+        $result = $stmt->rowCount();
+        $id = $stmt->fetch();
+        $stmt->closeCursor();
+        if ($result>0){
+            return $id['iduser'];
+        }
+        return 0;
+    }
 ?>
