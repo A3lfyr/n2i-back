57c635847c2b8d5eaa52d9ad8a6ed73c3dcf50a8
lacaulac <lacaulac@lacaulac.localdomain>
2020-12-04 04:39:00 +0100

    meh

--- utils/cors.php
+++ utils/cors.php
@@ -2,4 +2,9 @@
 header('Access-Control-Allow-Origin: *');
 
 header('Access-Control-Allow-Methods: GET, POST');
+
+if($_SERVER['REQUEST_METHOD'] == "OPTIONS") {
+    http_response_code(200);
+    exit(0);
+}
 ?>
