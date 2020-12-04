24824101ff7345c739d98e5db47f885f328b608d
lacaulac <lacaulac@lacaulac.localdomain>
2020-12-04 04:57:05 +0100

    meeeh

--- utils/cors.php
+++ utils/cors.php
@@ -5,6 +5,8 @@ header('Access-Control-Allow-Methods: GET, POST');
 
 header('Access-Control-Allow-Headers: *');
 
+header('Access-Control-Allow-Credentials: true');
+
 if($_SERVER['REQUEST_METHOD'] == "OPTIONS") {
     http_response_code(200);
     exit(0);
